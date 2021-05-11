4443695ce43ca191464a9c06b960a27d289e6507
MonHu <gakutyn.2020@gmail.com>
2021-05-11 17:07:20 +0900

    基本完成

--- app/script/main.js
+++ app/script/main.js
@@ -1,15 +1,37 @@
-$(() => {
-    new Audio("sound/startup.mp3").play();
+window.jQuery = window.$ = require('jquery');
+const startup_sound = new Audio("sound/startup.mp3");
+const send_sound = new Audio("sound/send.mp3");
+const reaction_sound = new Audio("sound/reaction.mp3");
+const error_sound = new Audio("sound/error.mp3");
+const { ipcRenderer } = require("electron");
+
+$(function () {
+    startup_sound.play();
+    ipcRenderer.send("onload");
+    ipcRenderer.on("theme", (e, arg) => {
+        document.documentElement.setAttribute("theme", arg);
+    });
+    $(window).scroll(function () {
+        $(".effect-fade").each(function () {
+            var elemPos = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > elemPos - windowHeight) {
+                $(this).addClass("effect-scroll");
+            }
+        });
+    });
+    jQuery(window).scroll();
 });
 
-$.getJSON("https://api.lainan.one/?msg=こんにちは", (data) => {
-    document.getElementById("reaction").innerHTML = data.reaction;
-});
-
-function getreac() {
-    $.getJSON("https://api.lainan.one/?msg=" + document.getElementById("text").value, (data) => {
-        document.getElementById("text").value = "";
-        document.getElementById("reaction").innerHTML = data.reaction;
-        new Audio("https://api.lainan.one/voice.wav?text=" + data.reaction).play();
+function send(text) {
+    send_sound.play();
+    $.getJSON("https://api.lainan.one/?msg=" + text, (data) => {
+        reaction_sound.play();
+        document.getElementById("message").value = "";
+        document.getElementById("content").innerHTML = data.reaction;
+        if (data.reaction.length >= 15) {
+            document.getElementById("content").style.fontSize = "1em";
+        };
     });
 };
--- index.js
+++ index.js
@@ -1,19 +1,26 @@
-const { app, BrowserWindow, screen } = require('electron');
+const { app, BrowserWindow, screen, nativeTheme, ipcMain } = require('electron');
 const path = require('path');
 
 function createWindow() {
     const win = new BrowserWindow({
-        width: screen.getPrimaryDisplay().size.width/1.2,
-        height: screen.getPrimaryDisplay().size.height/1.2,
+        width: screen.getPrimaryDisplay().size.width / 1.2,
+        height: screen.getPrimaryDisplay().size.height / 1.2,
         minWidth: 800,
         minHeight: 450,
-        backgroundColor: "#C5EDEA",
+        webPreferences: {
+            nodeIntegration: true
+        },
+        backgroundColor: "#808080",
         title: "Lainan"
     });
 
     win.setMenu(null);
     win.loadFile('app/index.html');
-}
+    win.openDevTools();
+    ipcMain.on('onload', (event, arg) => {
+        event.reply("theme", (nativeTheme.shouldUseDarkColors) ? "dark" : "light");
+    });
+};
 
 app.whenReady().then(() => {
     createWindow();
